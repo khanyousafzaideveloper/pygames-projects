252cae99970d8e245d7e99e3d86b5bfd5f5d3419
khanyousafzaideveloper <khanyousafzaideveloper@gmail.com>
2022-09-05 17:50:36 +0500

    draw function for displaying all images on window

--- imgs/main.py
+++ imgs/main.py
@@ -1,14 +1,15 @@
+from email.mime import image
 import pygame
 import math
 import time
 from utils import scale_image
 
-GRASS = pygame.image.load("imgs/grass.jpg")
-TRACK = pygame.image.load("imgs/track.png")
-TRACK_BORDER = pygame.image.load("imgs/track-border.png")
+GRASS = scale_image(pygame.image.load("imgs/grass.jpg"), 2.5)
+TRACK = scale_image(pygame.image.load("imgs/track.png"), 0.9)
+TRACK_BORDER = scale_image(pygame.image.load("imgs/track-border.png"), 0.9)
 FINISH = pygame.image.load("imgs/finish.png")
-RED_CAR = pygame.image.load("imgs/red-car.png")
-GREEN_CAR = pygame.image.load("imgs/green-car.png")
+RED_CAR = scale_image(pygame.image.load("imgs/red-car.png"), 0.55)
+GREEN_CAR = scale_image(pygame.image.load("imgs/green-car.png"), 0.55)
 
 WIDTH, HEIGHT = TRACK.get_width(), TRACK.get_height()
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -16,15 +17,17 @@ WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Racing Game!")
 FPS = 60
 
+def draw(win, images):
+    for img, pos in images:
+        win.blit(img, pos)
+
 run = True
 clock = pygame.time.Clock()
+images = [(GRASS, (0,0)), (TRACK, (0,0)) ]
 
 while run:
     clock.tick(FPS)
-
-    WIN.blit(GRASS, (0, 0))
-    WIN.blit(TRACK, (0,0))
-    WIN.blit(FINISH, (0,0))
+    draw(WIN, images)
 
     pygame.display.update()
 
