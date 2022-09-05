a0a24e8b47ba75620d0ff9b1a38840ba9b6038ad
khanyousafzaideveloper <khanyousafzaideveloper@gmail.com>
2022-09-05 15:15:45 +0500

    event to close the game

--- imgs/main.py
+++ imgs/main.py
@@ -1,20 +1,24 @@
+from operator import truediv
 import pygame
 import math
 import time
 
 GRASS = pygame.image.load("imgs/grass.jpg")
 TRACK = pygame.image.load("imgs/track.png")
-
 TRACK_BORDER = pygame.image.load("imgs/track-border.png")
-
 FINISH = pygame.image.load("imgs/finish.png")
-
 RED_CAR = pygame.image.load("imgs/red-car.png")
 GREEN_CAR = pygame.image.load("imgs/green-car.png")
 
 WIDTH, HEIGHT = TRACK.get_width(), TRACK.get_height()
-
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 
 pygame.display.set_caption("Racing Game!")
+
+run = True
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+            break
 
