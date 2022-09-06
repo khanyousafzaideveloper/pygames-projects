4ba0fa440792942344225c217ca8a05738e57493
khanyousafzaideveloper <khanyousafzaideveloper@gmail.com>
2022-09-06 23:00:38 +0500

    Finish line

--- imgs/main.py
+++ imgs/main.py
@@ -8,6 +8,7 @@ TRACK = scale_image(pygame.image.load("imgs/track.png"), 0.9)
 TRACK_BORDER = scale_image(pygame.image.load("imgs/track-border.png"), 0.9)
 TRACK_BORDER_MASK = pygame.mask.from_surface(TRACK_BORDER)
 FINISH = pygame.image.load("imgs/finish.png")
+FINISH_POSITIONS = (130, 250)
 RED_CAR = scale_image(pygame.image.load("imgs/red-car.png"), 0.55)
 GREEN_CAR = scale_image(pygame.image.load("imgs/green-car.png"), 0.55)
 
@@ -104,7 +105,7 @@ def move_player(player_car):
 
 run = True
 clock = pygame.time.Clock()
-images = [(GRASS, (0,0)), (TRACK, (0,0))]
+images = [(GRASS, (0,0)), (TRACK, (0,0)), (FINISH, FINISH_POSITIONS)]
 player_car = PlayerCar(4, 4)
 
 while run:
