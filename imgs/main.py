bd7ec8592df64810790ab37312766b141851765a
khanyousafzaideveloper <khanyousafzaideveloper@gmail.com>
2022-09-07 11:25:43 +0500

    computer_car

--- imgs/main.py
+++ imgs/main.py
@@ -80,11 +80,30 @@ class PlayerCar(AbstractCar):
         self.vel =- self.vel
         self.move()
 
-def draw(win, images, player_car):
+class ComputerCar(AbstractCar):
+    IMG = GREEN_CAR
+    START_POS = (180, 200)
+
+    def __init__(self, max_vel, rotation_vel, path=[]):
+        super().__init__(max_vel, rotation_vel)
+        self.path = path
+        self.current_point =0 
+        self.vel = max_vel
+
+    def draw_points(self, win):
+        for point in self.path:
+            pygame.draw.circle(win, (255, 0, 0), point)   
+
+    def draw(self, win):
+        super().draw(win)
+        self.draw_points(win)         
+
+def draw(win, images, player_car, computer_car):
     for img, pos in images:
         win.blit(img, pos)
     
     player_car.draw(win)
+    computer_car.draw(win)
     pygame.display.update()
 
 def move_player(player_car):
@@ -113,11 +132,12 @@ def move_player(player_car):
 run = True
 clock = pygame.time.Clock()
 images = [(GRASS, (0,0)), (TRACK, (0,0)), (FINISH, FINISH_POSITIONS), (TRACK_BORDER, (0,0))]
-player_car = PlayerCar(4, 4)
 
+player_car = PlayerCar(4, 4)
+computer_car = ComputerCar(4,4)
 while run:
     clock.tick(FPS)
-    draw(WIN, images, player_car)
+    draw(WIN, images, player_car, computer_car)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
